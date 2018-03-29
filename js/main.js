34b54ed2b97a8efa6cdb5dbc3a18470dce66325d
Chirath R <chirath.02@gmail.com>
2018-03-29 14:16:55 +0530

    toggle button on collapse

--- js/main.js
+++ js/main.js
@@ -1,35 +1,31 @@
-
-
 $("#description").popover({
-        trigger: "manual" ,
-        html: true,
-        animation: true
-    }).on("mouseenter", function () {
-        var _this = this;
-        $(this).popover("show");
-        $(".popover").on("mouseleave", function () {
-            $(_this).popover('hide');
-        });
-    }).on("mouseleave", function () {
-        var _this = this;
-        setTimeout(function () {
-            if (!$(".popover:hover").length) {
-                $(_this).popover("hide");
-            }
-        }, 300);
+    trigger: "manual",
+    html: true,
+    animation: true
+}).on("mouseenter", function () {
+    var _this = this;
+    $(this).popover("show");
+    $(".popover").on("mouseleave", function () {
+        $(_this).popover('hide');
+    });
+}).on("mouseleave", function () {
+    var _this = this;
+    setTimeout(function () {
+        if (!$(".popover:hover").length) {
+            $(_this).popover("hide");
+        }
+    }, 300);
 });
 
 
-$('#ready-to-upload-table').on('hide.bs.collapse', function () {
-  var icon = document.getElementById('ready-to-upload-arrow');
-  icon.classList.remove('fa-chevron-up');
-  icon.classList.add('fa-chevron-down');
+$('.toggle').on('click', function (e) {
+    var icon = e.target;
+    if (icon.classList.contains('fa-chevron-up')) {
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    }
+    else {
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
 });
-
-
-$('#ready-to-upload-table').on('show.bs.collapse', function () {
-  var icon = document.getElementById('ready-to-upload-arrow');
-  icon.classList.remove('fa-chevron-down');
-  icon.classList.add('fa-chevron-up');
-});
-
